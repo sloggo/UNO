caf1b9ab75dfdfe513c35e1cc7acf00d052e19ec
sloggo <70816908+sloggo@users.noreply.github.com>
2023-01-18 09:27:46 +0000

    Import JSON file into react
    Issue with importing JSON file with assertation json.
    Fixed by using just (from "./deck.json")

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import unoDeck from "./deck.json";
 
 export default function Board() {
-    const [deck, setDeck] = useState()  
+    const [deck, setDeck] = useState(unoDeck)  
+
+    useEffect(() =>{
+      console.log(unoDeck)
+    })
+
   return (
     <div>Board</div>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Board from './Components/Board';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
